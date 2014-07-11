828b5dcdc0dbc49b258f450f1f8b896ac89d81c2
jarppe <jarppe@metosin.fi>
2014-07-11 11:07:14 +0300

    basic EDN support

---
+++ src/compojure/api/edn.clj
@@ -0,0 +1,65 @@
+(ns compojure.api.edn
+  (:require [ring.util.http-response :refer [content-type bad-request!]]
+            [clojure.walk :refer [keywordize-keys]]
+            [clojure.java.io :as io]
+            [clojure.edn :as edn]))
+
+(defn edn-request?
+  "Checks from request content-type weather it's EDN."
+  [{:keys [content-type]}]
+  (and
+    content-type
+    (not (empty? (re-matches #"application/(vnd.+)?edn" content-type)))))
+
+(defn read-edn-body [body encoding]
+  (try
+    (with-open [in (-> body (io/reader :encoding encoding) (java.io.PushbackReader.))]
+      (edn/read in))
+    (catch RuntimeException e
+      (bad-request! {:type "edn-parse-exception" :message (.getMessage e)}))))
+
+(defn edn-request-support [handler {:keys [keywords?] :or {keywords? true}}]
+  (let [post-walk (if keywords? keywordize-keys identity)]
+    (fn [{:keys [character-encoding content-type body] :as request}]
+      (let [request (if-not (and body (edn-request? request))
+                      request
+                      (let [edn (-> body
+                                    (read-edn-body (or character-encoding "utf-8"))
+                                    (post-walk))
+                            request (assoc request :body        edn
+                                                   :body-params edn
+                                                   :edn-params  edn)]
+                        (if (map? edn)
+                          (update-in request [:params] merge edn)
+                          request)))
+            request (update-in request [:meta :consumes] concat ["application/edn"])]
+        (handler request)))))
+
+(defn serializable?
+  "Predicate that returns true whenever the response body is serializable."
+  [{:keys [body] :as response}]
+  (when response
+    (not (or
+           (instance? java.io.File body)
+           (instance? java.io.InputStream body)))))
+
+(defn ->edn [x]
+  (prn-str x))
+
+(defn ->edn-response [response]
+  (-> response
+      (content-type "application/edn; charset=utf-8")
+      (update-in [:body] ->edn)))
+
+(defn edn-response-support [handler opts]
+  (fn [request]
+    (let [request  (update-in request [:meta :produces] concat ["application/edn"])
+          response (handler request)]
+      (if (serializable? response)
+        (->edn-response response)
+        response))))
+
+(defn edn-support [handler & [opts]]
+  (-> handler
+      (edn-response-support opts)
+      (edn-request-support opts)))
---
+++ test/compojure/api/edn_test.clj
@@ -0,0 +1,61 @@
+(ns compojure.api.edn-test
+  (:require [midje.sweet :refer :all]
+            [ring.util.http-response :refer [ok]]
+            [clojure.java.io :as io]
+            [compojure.api.edn :refer :all])
+  (:import [java.io ByteArrayInputStream]))
+
+(defn stream [s]
+  (-> s (.getBytes "utf-8") (ByteArrayInputStream.) (io/reader) (java.io.PushbackReader.)))
+
+(defn edn-request [s]
+  {:content-type "application/edn"
+   :body (stream s)})
+
+(def with-edn-support (edn-support identity))
+
+(fact edn-request?
+  (edn-request? {:content-type "application/xml"})            => false
+  (edn-request? {:content-type "application/json"})           => false
+  (edn-request? {:content-type "application/vnd.myapp+json"}) => false
+  (edn-request? {:content-type "application/ednz"})           => false
+  (edn-request? {:content-type "application/edn"})            => true
+  (edn-request? {:content-type "application/vnd.myapp+edn"})  => true)
+
+(fact edn-support
+
+   (fact edn-request-support
+
+     (fact edn-list
+       (let [request (with-edn-support (edn-request "[1 2 3]"))]
+         (:body request)         => "[1 2 3]\n"
+         (:body-params request)  => [1 2 3]
+         (:edn-params request)   => [1 2 3]
+         (:json-params request)  => nil
+         (:params request)       => nil))
+
+     (fact edn-map-with-defaults
+       (let [request (with-edn-support (edn-request "{\"a\" 1 :b \"value\"}"))]
+         (:body request)         => "{:a 1, :b \"value\"}\n"
+         (:body-params request)  => {:a 1 :b "value"}
+         (:edn-params request)   => {:a 1 :b "value"}
+         (:params request)       => {:a 1 :b "value"}))
+     
+     (fact edn-map-with-np-keywords?
+       (let [request ((edn-support identity {:keywords? false}) (edn-request "{\"a\" 1 :b \"value\"}"))]
+         (:body request)         => "{\"a\" 1, :b \"value\"}\n"
+         (:body-params request)  => {"a" 1 :b "value"}
+         (:edn-params request)   => {"a" 1 :b "value"}
+         (:params request)       => {"a" 1 :b "value"}))
+
+     (fact edn-primitive
+       (let [request (with-edn-support (edn-request "true"))]
+         (:body request)         => "true\n"
+         (:body-params request)  => true
+         (:edn-params request)   => true
+         (:params request)       => nil))
+     
+     (fact edn-capability
+       (let [request (with-edn-support (edn-request "true" ))]
+         (-> request :meta :consumes) => ["application/edn"]
+         (-> request :meta :produces) => ["application/edn"]))))
